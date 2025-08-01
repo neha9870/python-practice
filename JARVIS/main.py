a25fd2edcc8fa144950db6537fa521608aab5c39
neha9870 <neha.dev.9870@gmail.com>
2025-08-01 20:17:07 +0530

    Add code in Jarvis

--- JARVIS/main.py
+++ JARVIS/main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
 import webbrowser
+import musicLibrary
 
 
 recognizer = sr.Recognizer()
@@ -9,24 +10,48 @@ engine = pyttsx3.init()
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)    
+
+
+
+    pass    
 
 if __name__ == "__main__":
-    speak("Hey Neha, how may I help you")
+    speak("Initializing Jarvis....")
     while True:
         # Listen for the wake word "Jarvis"
         # obtain audio from the microphone
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening...")
-            audio = r.listen(source,timeout=2)
+        
 
 
         print("recognizing...")
         try:
-            command = r.recognize_google(audio)
-            print(command)
-        except sr.UnknownValueError:
-            print("Sphinx could not understand audio")
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source,timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Ya")
+                #listen for command 
+            with sr.Microphone() as source :
+                print("Jarvis Active...") 
+                audio = r.listen(source)
+                command = r.recognize_google(audio) 
+                processCommand(command)
         except Exception as e:
             print("Error; {0}".format(e))            
 
+    
---
+++ JARVIS/musicLibrary.py
@@ -0,0 +1,11 @@
+music = {
+    "stealth":"https://www.youtube.com/watch?v=D_vAt1r1_VA&list=OLAK5uy_mlkqnqZOGyM2V-wvmEp8lDT23H6-EixM8",
+    "march" :"https://www.youtube.com/watch?v=Xqeq4b5u_Xw&list=RDXqeq4b5u_Xw&start_radio=1",
+    "jungle":"https://www.youtube.com/watch?v=TXi4XJK6DIk&list=RDTXi4XJK6DIk&start_radio=1",
+    "shiv":"https://www.youtube.com/watch?v=Onb6_bRJ0Bw&list=RDOnb6_bRJ0Bw&start_radio=1",
+    "sapphire":"https://www.youtube.com/watch?v=JgDNFQ2RaLQ&list=RDJgDNFQ2RaLQ&start_radio=1&pp=oAcB",
+    "doreamon":"https://www.youtube.com/watch?v=WLkkaBMU5EM&list=RDWLkkaBMU5EM&start_radio=1&pp=oAcB",
+    "aatma rama":"https://www.youtube.com/watch?v=pxCWiYFkvTg&list=RDpxCWiYFkvTg&start_radio=1&pp=oAcB",
+    "shyama":"https://www.youtube.com/watch?v=lm6CLMLSZBc&list=RDlm6CLMLSZBc&start_radio=1&pp=oAcB",
+    "ram":"https://www.youtube.com/watch?v=leVoi6kkp3o&list=RDleVoi6kkp3o&start_radio=1&pp=oAcB"
+}
